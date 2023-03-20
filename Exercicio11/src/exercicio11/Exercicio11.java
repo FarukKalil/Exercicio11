432e50a161af87696e0e6309ab74d00d5a1a804f
FarukKalil <faruk1.kalil@gmail.com>
2023-03-20 16:51:04 -0300

    Exercicio 11 finalizado

--- Exercicio11/src/exercicio11/Exercicio11.java
+++ Exercicio11/src/exercicio11/Exercicio11.java
@@ -1,8 +1,21 @@
 package exercicio11;
 
+import java.util.Scanner;
+
 public class Exercicio11 {
 
-public static void main(String[] args) {
-       
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        double quantidade_minima;
+        double quantidade_maxima;
+        double estoque_medio;
+        System.out.println("Calculadora de Estoque Medio");
+        System.out.println("Digite a quantidade media: ");
+        quantidade_minima = input.nextDouble();
+        
+        System.out.println("Digite a quantidade máxima: ");
+        quantidade_maxima = input.nextDouble();
+        estoque_medio = (quantidade_minima + quantidade_maxima) / 2;
+        System.out.println("Estoque Médio = " + estoque_medio);
     }
 }
